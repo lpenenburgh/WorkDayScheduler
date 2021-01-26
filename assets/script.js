e13d8b2f12eb63ec9b8d4579052584eb8043be3f
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-01-26 16:26:10 -0700

    colorCoding in js

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+
 
 
 //variables
@@ -7,7 +7,111 @@ var currentDay = document.getElementById("currentDay");
 
 
 //current date will display in the jumbotron, under the header
+
 $(currentDay).text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+//Depending on the time of the day; the hour blocks will change color
+var text9 = document.getElementById("9am-text");
+var text10 = document.getElementById("10am-text");
+var text11 = document.getElementById("11am-text");
+var text12 = document.getElementById("12pm-text");
+var text1 = document.getElementById("1pm-text");
+var text2 = document.getElementById("2pm-text");
+var text3 = document.getElementById("3pm-text");
+var text4 = document.getElementById("4pm-text");
+var text5 = document.getElementById("5pm-text");
+var text6 = document.getElementById("6pm-text");
+
+function colorCode () {
+    var currentTime = moment().format('hh:mm:ss a');
+    console.log(currentTime);
+
+    if (currentTime < '09:00am') {
+        text9.classList.add("future");    
+    } else if (currentTime > '09:00am' && currentTime < '10:00am') {
+        text9.classList.add("present");
+    } else {
+        text9.classList.add("past");
+    }
+
+    if (currentTime < '10:00am') {
+        text10.classList.add("future");    
+    } else if (currentTime > '10:00am' && currentTime < '11:00am') {
+        text10.classList.add("present");
+    } else {
+        text10.classList.add("past");
+    }
+
+    if (currentTime < '11:00am') {
+        text11.classList.add("future");    
+    } else if (currentTime > '11:00am' && currentTime < '12:00pm') {
+        text11.classList.add("present");
+    } else {
+        text11.classList.add("past");
+    }
+
+    if (currentTime < '12:00pm') {
+        text12.classList.add("future");    
+    } else if (currentTime > '12:00pm' && currentTime < '01:00pm') {
+        text12.classList.add("present");
+    } else {
+        text12.classList.add("past");
+    }
+
+    if (currentTime < '01:00pm') {
+        text1.classList.add("future");    
+    } else if (currentTime > '01:00pm' && currentTime < '02:00pm') {
+        text1.classList.add("present");
+    } else {
+        text1.classList.add("past");
+    }
+
+    if (currentTime < '02:00pm') {
+        text2.classList.add("future");    
+    } else if (currentTime > '02:00pm' && currentTime < '03:00pm') {
+        text2.classList.add("present");
+    } else {
+        text2.classList.add("past");
+    }
+
+    if (currentTime < '03:00pm') {
+        text3.classList.add("future");    
+    } else if (currentTime > '03:00pm' && currentTime < '04:00pm') {
+        text3.classList.add("present");
+    } else {
+        text3.classList.add("past");
+    }
+
+    if (currentTime < '04:00pm') {
+        text4.classList.add("future");    
+    } else if (currentTime > '04:00pm' && currentTime < '05:00pm') {
+        text4.classList.add("present");
+    } else {
+        text4.classList.add("past");
+    }
+
+    if (currentTime < '05:00pm') {
+        text5.classList.add("future");    
+    } else if (currentTime > '05:00pm' && currentTime < '06:00pm') {
+        text5.classList.add("present");
+    } else {
+        text5.classList.add("past");
+    }
+
+    if (currentTime < '06:00pm') {
+        text6.classList.add("future");    
+    } else if (currentTime > '06:00pm' && currentTime < '07:00pm') {
+        text6.classList.add("present");
+    } else {
+        text6.classList.add("past");
+    } 
+
+};
+
+//run color code function
+colorCode();
+
+
 
 // include 14 time blocks (9am-10pm) 
 //time blocks incude; the hour, an input area, and a save button
@@ -23,6 +127,5 @@ $(currentDay).text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
 
 
-});
 
 
