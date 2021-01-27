0147ac9f164b983015fceecd977bd9c70a0740d7
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-01-27 11:31:14 -0700

    local storage working

--- assets/script.js
+++ assets/script.js
@@ -1,109 +1,114 @@
 
 
 
-//variables
-var currentDay = document.getElementById("currentDay"); 
+
+
 
 
 
 //current date will display in the jumbotron, under the header
+var currentDay = document.getElementById("currentDay"); 
 
 $(currentDay).text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-//Depending on the time of the day; the hour blocks will change color
-var text9 = document.getElementById("9am-text");
-var text10 = document.getElementById("10am-text");
-var text11 = document.getElementById("11am-text");
-var text12 = document.getElementById("12pm-text");
-var text1 = document.getElementById("1pm-text");
-var text2 = document.getElementById("2pm-text");
-var text3 = document.getElementById("3pm-text");
-var text4 = document.getElementById("4pm-text");
-var text5 = document.getElementById("5pm-text");
-var text6 = document.getElementById("6pm-text");
 
+
+//textarea variables grabbed
+var text9Input = document.querySelector("#ninetext"); 
+var text10Input = document.querySelector("#tentext");
+var text11Input = document.querySelector("#eleventext");
+var text12Input = document.querySelector("#twelvetext");
+var text1Input = document.querySelector("#onetext");
+var text2Input = document.querySelector("#twotext");
+var text3Input = document.querySelector("#threetext");
+var text4Input = document.querySelector("#fourtext");
+var text5Input = document.querySelector("#fivetext");
+var text6Input = document.querySelector("#sixtext");
+
+
+//Depending on the time of the day; the hour blocks will change color
 function colorCode () {
     var currentTime = moment().format('hh:mm:ss a');
-    //console.log(currentTime);
+    console.log(currentTime);
 
     if (currentTime < '09:00am') {
-        text9.classList.add("future");    
+        text9Input.classList.add("future");    
     } else if (currentTime > '09:00am' && currentTime < '10:00am') {
-        text9.classList.add("present");
+        text9Input.classList.add("present");
     } else {
-        text9.classList.add("past");
+        text9Input.classList.add("past");
     }
 
     if (currentTime < '10:00am') {
-        text10.classList.add("future");    
+        text10Input.classList.add("future");    
     } else if (currentTime > '10:00am' && currentTime < '11:00am') {
-        text10.classList.add("present");
+        text10Input.classList.add("present");
     } else {
-        text10.classList.add("past");
+        text10Input.classList.add("past");
     }
 
     if (currentTime < '11:00am') {
-        text11.classList.add("future");    
+        text11Input.classList.add("future");    
     } else if (currentTime > '11:00am' && currentTime < '12:00pm') {
-        text11.classList.add("present");
+        text11Input.classList.add("present");
     } else {
-        text11.classList.add("past");
+        text11Input.classList.add("past");
     }
 
     if (currentTime < '12:00pm') {
-        text12.classList.add("future");    
+        text12Input.classList.add("future");    
     } else if (currentTime > '12:00pm' && currentTime < '01:00pm') {
-        text12.classList.add("present");
+        text12Input.classList.add("present");
     } else {
-        text12.classList.add("past");
+        text12Input.classList.add("past");
     }
 
     if (currentTime < '01:00pm') {
-        text1.classList.add("future");    
+        text1Input.classList.add("future");    
     } else if (currentTime > '01:00pm' && currentTime < '02:00pm') {
-        text1.classList.add("present");
+        text1Input.classList.add("present");
     } else {
-        text1.classList.add("past");
+        text1Input.classList.add("past");
     }
 
     if (currentTime < '02:00pm') {
-        text2.classList.add("future");    
+        text2Input.classList.add("future");    
     } else if (currentTime > '02:00pm' && currentTime < '03:00pm') {
-        text2.classList.add("present");
+        text2Input.classList.add("present");
     } else {
-        text2.classList.add("past");
+        text2Input.classList.add("past");
     }
 
     if (currentTime < '03:00pm') {
-        text3.classList.add("future");    
+        text3Input.classList.add("future");    
     } else if (currentTime > '03:00pm' && currentTime < '04:00pm') {
-        text3.classList.add("present");
+        text3Input.classList.add("present");
     } else {
-        text3.classList.add("past");
+        text3Input.classList.add("past");
     }
 
     if (currentTime < '04:00pm') {
-        text4.classList.add("future");    
+        text4Input.classList.add("future");    
     } else if (currentTime > '04:00pm' && currentTime < '05:00pm') {
-        text4.classList.add("present");
+        text4Input.classList.add("present");
     } else {
-        text4.classList.add("past");
+        text4Input.classList.add("past");
     }
 
     if (currentTime < '05:00pm') {
-        text5.classList.add("future");    
+        text5Input.classList.add("future");    
     } else if (currentTime > '05:00pm' && currentTime < '06:00pm') {
-        text5.classList.add("present");
+        text5Input.classList.add("present");
     } else {
-        text5.classList.add("past");
+        text5Input.classList.add("past");
     }
 
     if (currentTime < '06:00pm') {
-        text6.classList.add("future");    
+        text6Input.classList.add("future");    
     } else if (currentTime > '06:00pm' && currentTime < '07:00pm') {
-        text6.classList.add("present");
+        text6Input.classList.add("present");
     } else {
-        text6.classList.add("past");
+        text6Input.classList.add("past");
     } 
 
 };
@@ -111,23 +116,64 @@ function colorCode () {
 //run color code function
 colorCode();
 
+function init() {
+    renderUserInput();
+}
 
-var btn9 = document.querySelector("#save-button9");
-var btn10 = document.querySelectorAll("#save-button10");
-var btn11 = document.querySelectorAll("#save-button11");
-var btn12 = document.querySelectorAll("#save-button12");
-var btn1 = document.querySelectorAll("#save-button1");
-var btn2 = document.querySelectorAll("#save-button2");
-var btn3 = document.querySelectorAll("#save-button3");
-var btn4 = document.querySelectorAll("#save-button4");
-var btn5 = document.querySelectorAll("#save-button5");
-var btn6 = document.querySelectorAll("#save-button6");
 
-//time blocks are color coded depending on what hour in the day it is currently
 
-//save button works as an event listener- when it is clicked, the info put in the input area is saved to local storage
+function saveUserInput() {
+    var userInput = {
+        text9Input: text9Input.value.trim(),
+        text10Input: text10Input.value.trim(),
+        text11Input: text11Input.value.trim(),
+        text12Input: text12Input.value.trim(),
+        text1Input: text1Input.value.trim(),
+        text2Input: text2Input.value.trim(),
+        text3Input: text3Input.value.trim(),
+        text4Input: text4Input.value.trim(),
+        text5Input: text5Input.value.trim(),
+        text6Input: text6Input.value.trim()
+    };
 
-//when the page is refreshed, it pulls the input area info from local storage and saved on the page
+    localStorage.setItem("userInput", JSON.stringify(userInput));
+    console.log(userInput);
+}
+
+
+
+function renderUserInput() {
+    var lastInput = JSON.parse(localStorage.getItem("userInput"));
+
+    if (lastInput !== null) {
+        document.getElementById("ninetext").innerHTML = lastInput.text9Input;
+        document.getElementById("tentext").innerHTML = lastInput.text10Input;
+        document.getElementById("eleventext").innerHTML = lastInput.text11Input;
+        document.getElementById("twelvetext").innerHTML = lastInput.text12Input;
+        document.getElementById("onetext").innerHTML = lastInput.text1Input;
+        document.getElementById("twotext").innerHTML = lastInput.text2Input;
+        document.getElementById("threetext").innerHTML = lastInput.text3Input;
+        document.getElementById("fourtext").innerHTML = lastInput.text4Input;
+        document.getElementById("fivetext").innerHTML = lastInput.text5Input;
+        document.getElementById("sixtext").innerHTML = lastInput.text6Input;
+    } else {
+        return;
+    }
+};
+
+//buttons from html
+var saveButton = document.querySelectorAll("#savebutton");
+
+
+//event listener for save buttons. When clicked it will save user input to local storage, and then get from local storage to render on the page.
+$(saveButton).on("click", function(event) {
+    event.preventDefault();
+    saveUserInput();
+    renderUserInput();
+});
+
+//Fires when page is loaded, will also render user input on the html
+init()
 
 
 
